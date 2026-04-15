c460ac30d0398d185d28242312e0e2dd1bd1555f
S7nAcks <37936878+S7nAcks@users.noreply.github.com>
2026-04-14 20:08:34 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const CONFIG = {
+    EXPLORER: {
+        block: "https://mempool-explorer.nito.network/block/",
+        tx: "https://mempool-explorer.nito.network/tx/",
+        address: "https://mempool-explorer.nito.network/address/"
+    }
+};
