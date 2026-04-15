48face1242f73fec3dfc33b94ab908ef8778ec7a
S7nAcks <37936878+S7nAcks@users.noreply.github.com>
2026-04-14 20:39:58 -0700

    Delete public/config.js

--- public/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const CONFIG = {
-    EXPLORER: {
-        block: "https://mempool-explorer.nito.network/block/",
-        tx: "https://mempool-explorer.nito.network/tx/",
-        address: "https://mempool-explorer.nito.network/address/"
-    }
-};
